70b21a84dea84127938ee35dcc581955bae68c24
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-07 13:18:08 +0200

    Aggiungo consegna e sub task

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3
+// stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// Inizializzo una variabile stringa di appoggio
+// Loop che va da 1 a 100
+// Se il resto di i % 3 è 0 allora aggiungo Fizz alla variabile stringa
+// Se il resto di i % 5 è 0 allora aggiungo Buzz alla variabile stringa
+// Stampo la stringa e l'indice
